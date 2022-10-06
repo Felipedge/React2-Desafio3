42783b03403c9b45c1cd5a9ca7cb38a0f5fbf19a
Felipe P <fpalmasaldivia@gmail.com>
2022-10-05 22:45:17 -0300

    se modifica navbar

--- src/Views/Navbar.js
+++ src/Views/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import icono from '../Img/Pokemon.png';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './navbar.css';
 
@@ -6,6 +7,10 @@ const Navbar = () => {
     return (
         <nav class="navbar navbar-expand-lg bg-dark">
             <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img src={icono} alt="Logo" width="70"class="d-inline-block align-text-top"/>
+                </a>
+
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
                         <li class="nav-item">
@@ -14,7 +19,7 @@ const Navbar = () => {
                         <li class="nav-item">
                             <a class="nav-link link-success" href="#">Pokemones</a>
                         </li>
-                        
+
                     </ul>
                 </div>
             </div>
